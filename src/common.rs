b768e5494982a2c2482f7ed6ae3de117199192e0
sys1em <tmxiaoxie@163.com>
2025-09-02 04:43:44 +0800

    更新

--- src/common.rs
+++ src/common.rs
@@ -1024,7 +1024,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "http://rustdesk.sucaijun.com:21114".to_owned()
+    "https://rustdesk.sucaijun.com".to_owned()
 }
 
 #[inline]
